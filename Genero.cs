df42705040d41166acb73c8b0db00099b77f9e06
Thiago-com <82993812+Thiago-com@users.noreply.github.com>
2022-01-06 11:09:04 -0300

    Add files via upload

---
+++ Genero.cs
@@ -0,0 +1,19 @@
+namespace DIO.Series
+{
+    public enum Genero 
+    {
+        Ação = 1,
+        Aventura = 2,
+        Comédia = 3,
+        Documentário = 4,
+        Drama = 5,
+        Faroeste = 6,
+        Fantasia = 7,
+        Ficção_Cientifica = 8,
+        Musical = 9,
+        Suspense = 10,
+        Romance = 11,
+        Terror = 12,
+        Thriller = 13,
+    }
+}
